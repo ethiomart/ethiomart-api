266664aeafec58be7c69e52164b8e5e00495960b
Yohannes Adane <adaneyohannes11@gmail.com>
2026-02-28 22:52:35 +0300

    refactor: improve authentication middleware error handling

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -74,8 +74,8 @@ const verifyToken = async (req, res, next) => {
       id: user.id,
       email: user.email,
       role: user.role,
-      firstName: user.firstName,
-      lastName: user.lastName
+      firstName: user.first_name,
+      lastName: user.last_name
     };
 
     next();
@@ -161,8 +161,8 @@ const optionalAuth = async (req, res, next) => {
         id: user.id,
         email: user.email,
         role: user.role,
-        firstName: user.firstName,
-        lastName: user.lastName
+        firstName: user.first_name,
+        lastName: user.last_name
       };
     } else {
       req.user = null;
